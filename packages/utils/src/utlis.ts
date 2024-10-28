d2c781131dd8bd625a359e048ab9649d65a6eb06
东宇 郭 <738462315@qq.com>
2024-10-28 11:14:39 +0800

    chore: 测试一下，顺便传个东西

--- packages/utils/src/utlis.ts
+++ packages/utils/src/utlis.ts
@@ -1,14 +1,14 @@
-import { isObject } from "lodash-es";
+import { isObject } from 'lodash-es'
 
 function deepMerge<T = any>(src: any = {}, target: any = {}): T {
-  let key: string;
+  let key: string
   for (key in target) {
     src[key] =
       isObject(src[key]) && src[key] !== null
         ? deepMerge(src[key], target[key])
-        : (src[key] = target[key]);
+        : (src[key] = target[key])
   }
-  return src;
+  return src
 }
 
-export { deepMerge };
+export { deepMerge }
